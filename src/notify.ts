520d8f7444a230d2e3b8a117db46b9ee5350ebae
Victor Ciappina <vciappina@coursedog.com>
2022-12-14 00:38:18 +0100

    Refactor: Use pug templates for EmailClient.ts

--- src/notify.ts
+++ src/notify.ts
@@ -1,12 +1,16 @@
+import * as dotenv from 'dotenv';
 import { Player } from './models';
 import SecretSanta from './services/SecretSanta';
 
+dotenv.config();
+
 const players: Player[] = [
-  { name: 'Victor', email: 'victor_cl21@hotmail.com' },
+  { name: 'Victor', email: 'vecl21@gmail.com' },
   { name: 'Roraima', email: 'royadiro2@gmail.com' },
-  { name: 'Maigualida', email: 'maiguarod@gmail.com' },
-  { name: 'Yenissei', email: 'yenisseid@gmail.com' },
-  { name: 'Ricardo', email: 'rjdiaz154@gmail.com' },
+  // { name: 'Maigualida', email: 'maiguarod@gmail.com' },
+  // { name: 'Yenissei', email: 'yenisseid@gmail.com' },
+  // { name: 'Ricardo', email: 'rjdiaz154@gmail.com' },
+  // { name: 'Fernanda', email: 'rjdiaz154@gmail.com' },
 ];
 
 const secretSanta = new SecretSanta(players);
--- src/services/EmailClient.ts
+++ src/services/EmailClient.ts
@@ -1,52 +1,40 @@
-import nodemailer, { Transporter } from 'nodemailer';
-import path from 'path';
+import Email from 'email-templates';
 import { Player } from '../models';
-import CommandLine from './CommandLine';
-import HTMLParser from './HTMLParser';
 
 export default class EmailClient {
-  private transporter?: Transporter;
+  private email?: Email;
 
   private sender?: string;
-
-  private password?: string;
 
   constructor() {
     this.sendInfoToPlayer = this.sendInfoToPlayer.bind(this);
   }
 
   async init() {
-    const promptResult = await CommandLine.promptUser([
-      {
-        name: 'gmail',
-        validator: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-        warning: 'please enter a valid email account',
+    this.email = new Email({
+      message: {
+        from: `"VictorBot 👻" <${this.sender ?? 'victor@santabot.com'}>`,
       },
-      { name: 'password', hidden: true },
-    ]);
-    this.sender = promptResult.gmail;
-    this.password = promptResult.password;
-    this.transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: this.sender,
-        pass: this.password,
+      send: true,
+      preview: false,
+      transport: {
+        service: 'gmail',
+        auth: {
+          user: process.env.MAIL_USERNAME,
+          pass: process.env.MAIL_PASSWORD,
+        },
       },
     });
   }
 
   async sendInfoToPlayer(player: Player): Promise<void> {
-    const htmlParser = new HTMLParser(path.join(__dirname, '..', 'utils', 'emailTemplate.html'));
-    const message = htmlParser.getWithInjectedContent({
-      header: `Hola ${player.name},`,
-      secretBuddy: player.secretBuddy!,
-    });
-    await this.transporter?.sendMail({
-      from: `"VictorBot 👻" <${this.sender}>`,
-      to: `${player.name} <${player.email}>`,
-      subject: 'Tu amigo secreto está listo ✔',
-      replyTo: this.sender,
-      html: message,
+    await this.email?.send({
+      message: {
+        to: `${player.name} <${player.email}>`,
+        replyTo: this.sender,
+      },
+      template: 'secretSanta',
+      locals: player,
     });
   }
 }
