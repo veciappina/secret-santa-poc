7fa2ec24e63ed203acb2648ac85d057178ed349c
Victor Ciappina <vciappina@coursedog.com>
2023-12-21 20:08:48 +0100

    Feat: Implement forceBuddy functionality

--- src/models/player.ts
+++ src/models/player.ts
@@ -2,4 +2,5 @@ export interface Player {
   name: string;
   email: string;
   secretBuddy?: string;
+  forceBuddy?: string;
 }
--- src/services/SecretSanta.ts
+++ src/services/SecretSanta.ts
@@ -44,10 +44,34 @@ export default class SecretSanta {
     return this.getCircleCount() !== players.length;
   }
 
+  get forcedBuddies(): string[] {
+    return this.players.filter((p) => p.forceBuddy !== undefined).map((p) => p.forceBuddy!);
+  }
+
+  validateForceBuddies(): void {
+    const playersWithForceBuddies = this.players.filter((p) => p.forceBuddy !== undefined);
+    // eslint-disable-next-line no-restricted-syntax
+    for (const player of playersWithForceBuddies) {
+      if (player.name === player.forceBuddy)
+        throw new Error('A player cannot have itself as a forcedBuddy');
+    }
+    // detect if forcedBuddies contains duplicates
+    if (this.forcedBuddies.length !== new Set(this.forcedBuddies).size) {
+      throw new Error('There are duplicate forcedBuddies');
+    }
+    // sort players starting with those that have a forcedBuddy
+    this.players.sort((a, b) => {
+      if (a.forceBuddy !== undefined && b.forceBuddy === undefined) return -1;
+      if (a.forceBuddy === undefined && b.forceBuddy !== undefined) return 1;
+      return 0;
+    });
+  }
+
   assignSecretSantas(): void {
     const playersWithSecretSantaAssigned: Player[] = [];
     const indexOfPlayersWithSecretBuddyAssigned: number[] = [];
     const indexOfPlayersThatAreAlreadyReceivingGift: number[] = [];
+    this.validateForceBuddies();
 
     try {
       while (playersWithSecretSantaAssigned.length < this.players.length) {
@@ -56,12 +80,25 @@ export default class SecretSanta {
           indexOfPlayersWithSecretBuddyAssigned
         );
         const currentPlayer = this.players[currentPlayerIndex];
+        let secretSantaIndex: number;
 
-        const secretSantaIndex = getRandomNumber(this.players.length, [
-          ...new Set([currentPlayerIndex, ...indexOfPlayersThatAreAlreadyReceivingGift]),
-        ]);
+        if (currentPlayer.forceBuddy !== undefined) {
+          currentPlayer.secretBuddy = currentPlayer.forceBuddy;
+          secretSantaIndex = this.players.findIndex((p) => p.name === currentPlayer.forceBuddy);
+        } else {
+          const indexOfPlayersThatAreForcedBuddies = this.forcedBuddies.map((forcedBuddy) =>
+            this.players.map((p) => p.name).indexOf(forcedBuddy)
+          );
+          secretSantaIndex = getRandomNumber(this.players.length, [
+            ...new Set([
+              currentPlayerIndex,
+              ...indexOfPlayersThatAreAlreadyReceivingGift,
+              ...indexOfPlayersThatAreForcedBuddies,
+            ]),
+          ]);
+          currentPlayer.secretBuddy = this.players[secretSantaIndex].name;
+        }
 
-        currentPlayer.secretBuddy = this.players[secretSantaIndex].name;
         indexOfPlayersWithSecretBuddyAssigned.push(currentPlayerIndex);
         indexOfPlayersThatAreAlreadyReceivingGift.push(secretSantaIndex);
         playersWithSecretSantaAssigned.push(currentPlayer);
